5b260ffc0b91fe63ee572931a620ebeab3295368
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-08-03 15:45:21 -0600

    " -> 2 of 4 passed. pylint: 7.50/10 "

--- cspp1-assignments/m5/p1/perfect_cube.py
+++ cspp1-assignments/m5/p1/perfect_cube.py
@@ -13,6 +13,6 @@ def main():
     guess = 0
     while guess**3 < num:
         guess += 1
-    print(guess, 'is a perfect cube')
+    print(num, 'is a perfect cube')
 if __name__ == "__main__":
     main()
