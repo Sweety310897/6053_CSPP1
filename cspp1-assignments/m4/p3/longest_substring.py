5a079953c9172e845e8a34c49682837a910a5abc
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-08-02 20:44:18 -0600

    " -> 4 of 5 passed. pylint: 9.33/10 "

--- cspp1-assignments/m4/p3/longest_substring.py
+++ cspp1-assignments/m4/p3/longest_substring.py
@@ -13,19 +13,18 @@ If you have time, come back to this problem after you've had a break
 and cleared your head.'''
 def main():
     #this is main program
-str_abc = input()
-maxlength = 0
-initialvalue = str_abc[0]
-longest = str_abc[0]
-for i in range(len(str_abc)-1):
-    if str_abc[i+1] > str_abc[i]:
-        initialvalue += str_abc[i+1]
-        if len(initialvalue) > maxlength:
-                 maxlength = len(initialvalue)
-                 longest = initialvalue
-    else:
-        initialvalue = str_abc[i+1]
-    i += 1
-print(longest)
+    str_abc = input()
+    maxlength = 0
+    initialvalue = str_abc[0]
+    longest = str_abc[0]
+    for i in range(len(str_abc)-1):
+        if str_abc[i+1] > str_abc[i]:
+            initialvalue += str_abc[i+1]
+            if len(initialvalue) > maxlength:
+                maxlength = len(initialvalue)
+                longest = initialvalue
+        else:
+            initialvalue = str_abc[i+1]
+    print(longest)
 if __name__ == "__main__":
     main()
