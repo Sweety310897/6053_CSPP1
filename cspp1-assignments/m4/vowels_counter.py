0f4eaf2640bcff00886cc94e76fe7fe0e3f4d84d
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-08-02 19:09:16 -0600

    " -> 0 of 1 passed. pylint: 6.00/10 "

--- cspp1-assignments/m4/vowels_counter.py
+++
@@ -1,7 +0,0 @@
-''' This is vowels coun program '''
-STRING1 = input('enter the string')
-COUNT = 0
-for char in STRING1:
-    if (char == 'a' or char == 'e' or char == 'i' or char == 'o' or char == 'u'):
-        COUNT += 1
-print(COUNT)
