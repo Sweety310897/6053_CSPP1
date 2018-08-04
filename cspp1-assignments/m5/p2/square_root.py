4488593ee30f96f2bb4790dba2ee5c03bdbb2d20
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-08-03 18:35:01 -0600

    " -> 0 of 3 passed. pylint: 10.00/10 "

---
+++ cspp1-assignments/m5/p2/square_root.py
@@ -0,0 +1,29 @@
+'''This is 2nd pgm'''
+# Write a python program to find the square root of the given number
+# using approximation method
+
+# testcase 1
+# input: 25
+# output: 4.999999999999998
+
+# testcase 2
+# input: 49
+# output: 6.999999999999991
+
+def main():
+    '''this is 2nd program'''
+    num = int(input('enter square root'))
+    epsilon = 0.001
+    guess = 0.01
+    increment = 0.0001
+    num_guesses = 0
+    while abs(guess**2-num) >= epsilon:
+        guess += increment
+        num_guesses = 1
+    print(num_guesses)
+    if abs((guess**2) - num) >= epsilon:
+        print(num)
+    else:
+        print(guess, 'guess is close')
+if __name__ == "__main__":
+    main()
