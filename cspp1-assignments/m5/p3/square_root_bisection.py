0e477469029bc4ec1a4a04af59fdb3579593d8f8
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-08-03 18:45:39 -0600

    " -> 0 of 3 passed. pylint: 10.00/10 "

---
+++ cspp1-assignments/m5/p3/square_root_bisection.py
@@ -0,0 +1,28 @@
+'''This is 3rd pgm'''
+# Write a python program to find the square root of the given number
+# using approximation method
+# testcase 1
+# input: 25
+# output: 4.999999999999998
+# testcase 2
+# input: 49
+# output: 6.999999999999991
+def main():
+    ''' This is 3rd pgm'''
+    num1 = abs(float(input('enter num')))
+    low = abs(float(input('lower value')))
+    high = abs(float(input('higher value')))
+    epsilon = 0.001
+    while True:
+        mid = (low+high)/2
+        difference = mid**2-num1
+        if abs(difference) <= epsilon:
+            break
+        if difference < 0:
+            low = mid
+        else:
+            high = mid
+    print(round(mid, 3))
+
+if __name__ == "__main__":
+    main()
