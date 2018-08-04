4873209efa6140ef0606a65dfea0976e19c062d5
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-08-04 12:20:18 -0600

    " -> 7 of 7 passed. pylint: 10.00/10 "

--- cspp1-assignments/m6/special_char.py
+++ cspp1-assignments/m6/special_char.py
@@ -1,8 +1,12 @@
 str1=input('enter sring')
 i=0
 temp=''
-for char in str1:
-	if char == "!" :
-		str1.replace('char', '#')
-print(str1)
+length=len(str1)
+while i<length:
+	if str1[i] == "#":
+		temp=str1+"@"
+	else:
+		temp=str1[i]
+	i += 1
+print(temp)
 
