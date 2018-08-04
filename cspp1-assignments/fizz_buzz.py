32ecca0bc3def84b33a1335a68898e63e0f3db65
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-08-04 11:50:48 -0600

    " -> 4 of 7 passed. pylint: 10.00/10 "

--- cspp1-assignments/fizz_buzz.py
+++ cspp1-assignments/fizz_buzz.py
@@ -1,12 +1,12 @@
 num=int(input('enter num'))
 i=1
 while i<=num:
-	if i%3==0:
-		print('Fizz')
-	elif i%5==0:
-		print('Buzz')
-	elif i%3==0 and i%5==0:
+	if i%3==0 and i%5==0:
 		print('FizzBuzz')
+	elif i%3==0:
+		print('Fizz')
+	elif i%5==0
+		print('Buzz')
 	else:
 		print(i)
 	i += 1
--- cspp1-assignments/m6/special_char.py
+++ cspp1-assignments/m6/special_char.py
@@ -3,6 +3,6 @@ i=0
 temp=''
 for char in str1:
 	if char == "!" :
-		str1.replace('!')
+		str1.replace('char', '#')
 print(str1)
 
