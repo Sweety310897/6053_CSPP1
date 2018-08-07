385191f23c8fc21d2dcfbaef86191997f34e6649
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-08-06 21:01:15 -0600

    " -> 0 of 10 passed. pylint: 9.60/10 "

--- cspp1-assignments/m7/p3/assignment3.py
+++ cspp1-assignments/m7/p3/assignment3.py
@@ -69,6 +69,6 @@ def main():
     # data = "4773 0.2"
     data = data.split(' ')
     data = list(map(float, data))
-    print('lowest payment rate:', payingdebtoffina_year(data[0], data[1]))
+    print('lowest payment:', payingdebtoffina_year(data[0], data[1]))
 if __name__ == "__main__":
     main()
