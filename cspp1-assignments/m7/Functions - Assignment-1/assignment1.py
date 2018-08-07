c0d3701cf0922404fa6b85452219dda5eb718af5
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-08-06 18:43:12 -0600

    " -> 10 of 10 passed. pylint: 10.00/10 "

--- cspp1-assignments/m7/Functions - Assignment-1/assignment1.py
+++ cspp1-assignments/m7/Functions - Assignment-1/assignment1.py
@@ -36,6 +36,6 @@ def main():
     data = input()
     data = data.split(' ')
     data = list(map(float, data))
-    print('Remaining balance', payingdebtoffina_year(data[0], data[1], data[2]))
+    print('Remaining balance:', payingdebtoffina_year(data[0], data[1], data[2]))
 if __name__ == "__main__":
     main()
