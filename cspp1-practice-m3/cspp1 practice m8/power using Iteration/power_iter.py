10eda0363524e2ea990ad670d1eed50b531acc5e
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-08-07 19:12:47 -0600

    " -> 10 of 10 passed. pylint: 10.00/10 "

--- cspp1-practice-m3/cspp1 practice m8/power using Iteration/power_iter.py
+++ cspp1-practice-m3/cspp1 practice m8/power using Iteration/power_iter.py
@@ -1,8 +1,9 @@
+'''This is practice pgm'''
 # Exercise: PowerIter
-# Write a Python function, iterPower(base, exp), 
+# Write a Python function, iterPower(base, exp),
 #that takes in two numbers and returns the base^(exp) of given base and exp.
 # This function takes in two numbers and returns one number.
-def iterPower(base, exp):
+def iterpowe_r(base, exp):
     '''
     base: int or float.
     exp: int >= 0
@@ -10,12 +11,13 @@ def iterPower(base, exp):
     '''
     result = 1
     while exp >= 1:
-    	result = result * base
-    	exp -= 1
+        result = result * base
+        exp -= 1
     return result
 def main():
+    '''this is main function'''
     data = input()
     data = data.split()
-    print(iterPower(float(data[0]),int(data[1]))) 
+    print(iterpowe_r(float(data[0]), int(data[1])))
 if __name__ == "__main__":
     main()
