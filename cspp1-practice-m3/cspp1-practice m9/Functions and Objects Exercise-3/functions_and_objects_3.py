4c677e2a01b7585d3d72c044a9dd6e7f59b39fa0
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-08-08 19:45:51 -0600

    3rd activitymod9

--- cspp1-practice-m3/cspp1-practice m9/Functions and Objects Exercise-3/functions_and_objects_3.py
+++ cspp1-practice-m3/cspp1-practice m9/Functions and Objects Exercise-3/functions_and_objects_3.py
@@ -1,20 +1,32 @@
+'''This is 3rd activity'''
 #Exercise : Function and Objects Exercise-3
 #Implement a function that converts the given testList = [1, -4, 8, -9] into [1, 16, 64, 81]
-def apply_to_each(L, f):
+def apply_to_each(lis_t, functio_n):
+    '''
+    List is given and square of function is performed
+     and value is returned
+    '''
     i = 0
-    length = len(L)
+    length = len(lis_t)
     while i < length:
-        L[i] = L[i] * L[i]
+        lis_t[i] = lis_t[i] * lis_t[i]
         i += 1
-    return L
+    return lis_t
 def square(num):
+    '''
+    input number
+    output square of that number
+    '''
     return num * num  
 def main():
+    '''
+    This is main function
+    '''
     data = input()
     data = data.split()
     list1 = []
     for j in data:
         list1.append(int(j))
     print(apply_to_each(list1, square))
-if __name__== "__main__":
+if __name__ == "__main__":
     main()
