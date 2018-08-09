a0139d515ae2372eb1fd95520518ef2c56b32fa6
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-08-08 19:25:47 -0600

    odd tuples

--- cspp1-practice-m3/cspp1-practice m9/Odd Tuples Exercise/odd_tuples.py
+++ cspp1-practice-m3/cspp1-practice m9/Odd Tuples Exercise/odd_tuples.py
@@ -1,20 +1,22 @@
+'''This is odd strings pgm'''
 #Exercise : Odd Tuples
-#Write a python function oddTuples(aTup) that takes a some numbers in the tuple as input and returns a tuple in which contains odd index values in the input tuple
-def oddTuples(aTup):
+#Write a python function oddTuples(aTup) that takes a some numbers
+# in the tuple as input and returns a tuple in which contains odd index values in the input tuple
+def oddtuple_s(atu_p):
     '''
     aTup: a tuple
-    input : aTupu
-    output: odd range of aTup 
-    returns: tuple, every other element of aTup. 
+    input : aTup
+    output: odd range of aTup
+    returns: tuple, every other element of aTup.
     '''
-    return aTup[::2]
+    return atu_p[::2]
 def main():
     '''this is main function'''
     data = input()
     data = data.split()
-    aTup = ()
+    atu_p = ()
     for j in range(len(data)):
-        aTup += ((data[j]),)
-    print(oddTuples(aTup))
+        atu_p += ((data[j]),)
+    print(oddtuple_s(atu_p))
 if __name__ == "__main__":
     main()
