251777dcfc5a483a40f2a13bc9d234027ebbf0cf
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-08-14 21:17:48 -0600

    " -> 2 of 3 passed. pylint: 7.08/10 "

--- cspp1-assignments/m14/CodeCampPoker/poker.py
+++ cspp1-assignments/m14/CodeCampPoker/poker.py
@@ -66,8 +66,19 @@ def is_flush(hand):
         return True
     return False
     '''
-    set1=set([suite for face,suite in hand])
-    return len(set1) == 1
+    is_flush_=1
+    i = 0
+    j = 1
+    while j < len(hand) and is_flush_==1:
+        if hand[i][1]!= hand[j][1]:
+            is_flush_=0
+        else:
+            is_flush_=1
+        i += 1
+        j += 1
+    return is_flush_== 1
+        
+    
 
 def hand_rank(hand):
     '''
