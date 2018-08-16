4f34d7f5e27d297aaa17432ea943550dd9f5e27e
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-08-16 14:40:42 -0600

    " -> 1 of 19 passed. pylint: 6.29/10 "

--- cspp1-assignments/poker/poker1/CodeCampPoker/poker.py
+++ cspp1-assignments/poker/poker1/CodeCampPoker/poker.py
@@ -14,41 +14,7 @@ def is_straight(hand):
         Think of an algorithm: given the card face value how to check if it a straight
         Write the code for it and return True if it is a straight else return False
     '''
-    i = 0
-    temp = []
-    while i < len(hand):
-        if hand[i][0] == 'T':
-            k = hand[i][0].replace("T", "10")
-            temp.append(int(k))
-        elif hand[i][0] == 'J':
-            k = hand[i][0].replace("J", "11")
-            temp.append(int(k))
-        elif hand[i][0] == 'Q':
-            k = hand[i][0].replace("Q", "12")
-            temp.append(int(k))
-        elif hand[i][0] == 'K':
-            k = hand[i][0].replace("K", "13")
-            temp.append(int(k))
-        elif hand[i][0] == 'A':
-            k = hand[i][0].replace("A", "14")
-            temp.append(int(k))
-        else:
-            k = hand[i][0]
-            temp.append(int(k))
-        i += 1
-    temp1 = sorted(temp)
-    p_tr = 0
-    k_tr = 1
-    cout1 = 1
-    while k_tr < len(temp1) and cout1 == 1:
-        if temp1[k_tr]-temp1[p_tr] == 1:
-            pass
-        else:
-            cout1 = 0
-        k_tr += 1
-        p_tr += 1
-    return cout1 == 1
-
+    pass
 
 def is_flush(hand):
     '''
@@ -59,86 +25,7 @@ def is_flush(hand):
         Think of an algorithm: given the card suite how to check if it is a flush
         Write the code for it and return True if it is a flush else return False
     '''
-    is_flush_1 = 1
-    i = 0
-    j = 1
-    while j < len(hand) and is_flush_1 == 1:
-        if hand[i][1] != hand[j][1]:
-            is_flush_1 = 0
-        else:
-            is_flush_1 = 1
-        i += 1
-        j += 1
-    return is_flush_1 == 1
-
-def is_four_of_a_kind(hands):
-    list2=[]
-    temp=[]
-    for card in hands:
-        list2.append(card[0])
-    #print(list2)
-    for x in list2:
-        temp.append(list2.count(x))
-    if max(temp) == 4:
-        return True
-    else:
-        return False
-    
-    
-def is_three_of_a_kind(hands):
-    list2=[]
-    temp=[]
-    for card in hands:
-        list2.append(card[0])
-    #print(list2)
-    for x in list2:
-        temp.append(list2.count(x))
-    if max(temp) == 3:
-        return True
-    else:
-        return False
-
-
-def is_one_pair(hands):
-    list2=[]
-    temp=[]
-    for card in hands:
-        list2.append(card[0])
-    #print(list2)
-    for x in list2:
-        temp.append(list2.count(x))
-    if max(temp) == 2:
-        return True
-    else:
-        return False
-
-def is_full_house(hands):
-    list2=[]
-    temp=[]
-    for card in hands:
-        list2.append(card[0])
-     
-    print(list2)
-    temp1=sorted(list2)
-
-    if temp1[0]==temp1[1]==temp1[2]:
-        if temp1[3]==temp1[4]:
-            return True
-    if temp1[0]==temp1[1]:
-        if temp1[2]==temp1[3]==temp1[4]:
-            return True
-    else:
-        return False
-
-def is_two_pair(hands):
-    list2=[]
-    temp=[]
-    for card in hands:
-        list2.append(card[0])
-    print(list2[0])
-    
-    
-
+    pass
 
 def hand_rank(hand):
     '''
@@ -164,28 +51,7 @@ def hand_rank(hand):
     # third would be a straight with the return value 1
     # any other hand would be the fourth best with the return value 0
     # max in poker function uses these return values to select the best hand
-    is_straight_1 = is_straight(hand)
-    is_flush_1 = is_flush(hand)
-
-    if is_flush_1 and is_straight_1:
-        return 9
-    elif is_four_of_a_kind(hand):
-        return 8
-    elif is_full_house(hand):
-        return 7
-    elif is_three_of_a_kind(hand):
-        return 3
-    elif is_two_pair(hand):
-        return 2
-    elif is_one_pair(hand):
-        return 2
-    elif is_straight_1:
-        return 1
-    
-    elif is_flush_1:
-        return 2
-    else:
-        return 0
+    return 1
 
 def poker(hands):
     '''
@@ -218,4 +84,3 @@ if __name__ == "__main__":
         HANDS.append(ha)
     # test the poker function to see how it works
     print(' '.join(poker(HANDS)))
-    
