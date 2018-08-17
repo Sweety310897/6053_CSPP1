2c5c9c13091da4ec288d44092736377eb3246ca8
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-08-16 21:30:13 -0600

    " -> 0 of 19 passed. pylint: 6.48/10 "

--- cspp1-assignments/poker1/CodeCampPoker/poker12.py
+++ cspp1-assignments/poker1/CodeCampPoker/poker12.py
@@ -218,6 +218,7 @@ if __name__ == "__main__":
     HANDS = []
     for x in range(COUNT):
         line = input()
+        print(line)
         ha = line.split(" ")
         HANDS.append(ha)
     # test the poker function to see how it works
