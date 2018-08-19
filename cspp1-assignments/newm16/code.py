ea3834ecf02657a06e5b432c933699bb11c01174
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-08-19 00:49:39 -0600

    " -> 0 of 3 passed. pylint: 0/0 "

--- cspp1-assignments/newm16/code.py
+++ cspp1-assignments/newm16/code.py
@@ -1,77 +1,18 @@
-'''
-    Document Distance - A detailed description is given in the PDF
-'''
-import re
-import math
-#filename = "stopwords.txt"
-def similarity(dict1, dict2):
-    '''
-        Compute the document distance as given in the PDF
-    '''
-    temp1 = dict1.lower()
-    temp2 = dict2.lower()
-    temp5 = []
-    temp6 = []
-    temp10 = []
-    temp11 = []
-    for element1 in temp1:
-        temp5.append(element1.strip())
-    for element2 in temp2:
-        temp6.append(element2.strip())
-    temp12 = str(temp5)
-    temp13 = str(temp6)
-    temp3 = temp12.split()
-    temp4 = temp13.split()
-    for word in temp3:
-        temp10.append(re.sub('[^ a-z]', "", word))
-    #print(temp10)
-    for word in temp4:
-        temp11.append(re.sub('[^ a-z]', "", word))
-    stopwords = load_stopwords("stopwords.txt")
-    temp7 = temp10[:]
-    temp8 = temp11[:]
-    for word in temp7:
-        if word in stopwords and len(word) > 0:
-            temp10.remove(word)
-    for word in temp8:
-        if word in stopwords:
-            temp11.remove(word)
-    #print("temp10",temp10,"temp11",temp11)
-    #print(temp10)
-    #print(temp11)
-    dictionary1 = {}
-    for word in temp10:
-        if word not in dictionary1:
-            dictionary1[word] = 1
-        else:
-            dictionary1[word] += 1
-    dictionary2 = {}
-    # print("di1",dictionary1,"dic2",dictionary2)
-    for word in temp11:
-        if word not in dictionary2:
-            dictionary2[word] = 1
-        else:
-            dictionary2[word] += 1
-    keys = list(dictionary1.keys())+list(dictionary2.keys())
-    #print(keys)
-    for word in keys:
-        if not word:
-            keys.remove(word)
-    #print(keys)
-    dictionary3 = {}
-    for k in keys:
-        dictionary3[k] = [0, 0]
-    #print(dictionary3)
-    for var in dictionary1:
-        dictionary3[var][0] = dictionary1[var]
-    for var in dictionary2:
-        dictionary3[var][1] = dictionary2[var]
-    #print(dictionary3)
-    #print(dictionary3[1])
-    # for everykey in dictionary3.keys:
-    #     sum1 = 0
-    #     sum2=0
-    sum1 = 0
+def combine_dictionaries(dictionary_one,dictionary_two):
+	dictionary = {}
+	for everyword in dictionary_one:
+		if everyword in dictionary_two:
+			dictionary[everyword] = [dictionary_one[everyword],dictionary_two[everyword]]
+	for everyword in dictionary_one:
+		if everyword not in dictionary:
+			dictionary[everyword] = [dictionary_one[everyword],0]
+	for everyword in dictionary_two:
+		if everyword not in dictionary:
+			dictionary[everyword] = [0,dictionary_two[everyword]]
+	return dictionary
+
+def calculate_similarity(dictionary_values):
+	sum1 = 0
     sum2 = 0
     numerator = 0
     for everykey in dictionary3.keys():
@@ -84,17 +25,31 @@ def similarity(dict1, dict2):
 
 
 
+def create_dictionary(words_list):
+	dictionary = {}
+	stopwords = load_stopwords("stopwords.txt")
+	for word in words_list:
+		word = word.strip()
+		if word not in stopwords and len(words) > 0:
+			if word not in dictionary:
+				dictionary[word] = 1
+			else:
+				dictionary[word] += 1
+	return dictionary 
 
-        # numerator = dictionary3[everykey][0] * dictionary3[everykey][1]
-        # for i in range(denominator1):
-        #     denominator1 = (dictionary3[everykey][0]**2)
-        #     sum1 = sum1 + denominator1[i]
-        # temp19 = math.sqrt(sum1)
-        # for i in range(denominator2):
-        #     denominator2 = (dictionary3[everykey][1]**2)
-        #     sum2=sum2+denominator2[i]
-        # temp20 = math.sqrt(sum2)
 
+def clean_text(text_input):
+	words = text_input.lower().strip().replace('\'','')
+	regex = re.compile('[^a-z]')
+	words = regex.sub(" ",words).split(" ")
+	return words
+
+
+def similarity(text_input_one,text_input_two):
+	dictionary_one = create_dictionary(clean_text(text_input_one))
+	dictionary_two = create_dictionary(clean_text(text_input_two))
+	dictionary = combine_dictionaries(dictionary_one,dictionary_two)
+	return calculate_similarity(dictionary)
 
 
 def load_stopwords(filename):
@@ -118,4 +73,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
