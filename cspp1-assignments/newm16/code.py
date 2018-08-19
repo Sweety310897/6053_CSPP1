5705e2b72d999f5459c14c6e00d7dd3118d45360
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-08-19 00:50:34 -0600

    " -> 0 of 3 passed. pylint: -1.67/10 "

--- cspp1-assignments/newm16/code.py
+++ cspp1-assignments/newm16/code.py
@@ -1,18 +1,19 @@
 def combine_dictionaries(dictionary_one,dictionary_two):
-	dictionary = {}
-	for everyword in dictionary_one:
-		if everyword in dictionary_two:
-			dictionary[everyword] = [dictionary_one[everyword],dictionary_two[everyword]]
-	for everyword in dictionary_one:
-		if everyword not in dictionary:
-			dictionary[everyword] = [dictionary_one[everyword],0]
-	for everyword in dictionary_two:
-		if everyword not in dictionary:
-			dictionary[everyword] = [0,dictionary_two[everyword]]
-	return dictionary
+    dictionary = {}
+    for everyword in dictionary_one:
+        if everyword in dictionary_two:
+            dictionary[everyword] = [dictionary_one[everyword],dictionary_two[everyword]]
+    for everyword in dictionary_one:
+        if everyword not in dictionary:
+            dictionary[everyword] = [dictionary_one[everyword],0]
+    for everyword in dictionary_two:
+        if everyword not in dictionary:
+            dictionary[everyword] = [0,dictionary_two[everyword]]
+    return dictionary
 
 def calculate_similarity(dictionary_values):
-	sum1 = 0
+
+    sum1 = 0
     sum2 = 0
     numerator = 0
     for everykey in dictionary3.keys():
@@ -26,30 +27,30 @@ def calculate_similarity(dictionary_values):
 
 
 def create_dictionary(words_list):
-	dictionary = {}
-	stopwords = load_stopwords("stopwords.txt")
-	for word in words_list:
-		word = word.strip()
-		if word not in stopwords and len(words) > 0:
-			if word not in dictionary:
-				dictionary[word] = 1
-			else:
-				dictionary[word] += 1
-	return dictionary 
+    dictionary = {}
+    stopwords = load_stopwords("stopwords.txt")
+    for word in words_list:
+        word = word.strip()
+        if word not in stopwords and len(words) > 0:
+            if word not in dictionary:
+                dictionary[word] = 1
+            else:
+                dictionary[word] += 1
+    return dictionary 
 
 
 def clean_text(text_input):
-	words = text_input.lower().strip().replace('\'','')
-	regex = re.compile('[^a-z]')
-	words = regex.sub(" ",words).split(" ")
-	return words
+    words = text_input.lower().strip().replace('\'','')
+    regex = re.compile('[^a-z]')
+    words = regex.sub(" ",words).split(" ")
+    return words
 
 
 def similarity(text_input_one,text_input_two):
-	dictionary_one = create_dictionary(clean_text(text_input_one))
-	dictionary_two = create_dictionary(clean_text(text_input_two))
-	dictionary = combine_dictionaries(dictionary_one,dictionary_two)
-	return calculate_similarity(dictionary)
+    dictionary_one = create_dictionary(clean_text(text_input_one))
+    dictionary_two = create_dictionary(clean_text(text_input_two))
+    dictionary = combine_dictionaries(dictionary_one,dictionary_two)
+    return calculate_similarity(dictionary)
 
 
 def load_stopwords(filename):
