dc2a5c3b33f1e14e00b4b9ffed15c50bf37298c9
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-08-24 17:34:29 -0600

    " -> 10 of 14 passed. pylint: 7.19/10 "

--- cspp1-assignments/m21/Tictactoe.py
+++ cspp1-assignments/m21/Tictactoe.py
@@ -1,86 +1,86 @@
 def tictactoe():
-	temp3 = []
-	# rowcheck = ''
-	# colcheck = ''
-	# diagcheck = ''
-	winner = None
-	for i in range(3):
-		temp1 = input()
-		temp2 = temp1.split()
-		temp3.append(temp2)
+    temp3 = []
+    # rowcheck = ''
+    # colcheck = ''
+    # diagcheck = ''
+    winner = None
+    for i in range(3):
+        temp1 = input()
+        temp2 = temp1.split()
+        temp3.append(temp2)
 
-	is_valid = is_valid_input(temp3) and is_valid_game(temp3)
-	if is_valid:
-		winner = rowchec_k(temp3)
-		if winner == None:
-			winner = colchec_k(temp3)
-		if winner == None:
-			winner = diag_check(temp3)
-		if winner == None:
-			winner = "draw"
-		
-		return winner
+    is_valid = is_valid_input(temp3) and is_valid_game(temp3)
+    if is_valid:
+        winner = rowchec_k(temp3)
+        if winner == None:
+            winner = colchec_k(temp3)
+        if winner == None:
+            winner = diag_check(temp3)
+        if winner == None:
+            winner = "draw"
+        
+        return winner
 
 
-	else:
-		if is_valid_input(temp3) == True and is_valid_game(temp3) == False:
-			winner = "invalid game"
-		else:
-			winner = "invalid input"
-		return winner
+    else:
+        if is_valid_input(temp3) == True and is_valid_game(temp3) == False:
+            winner = "invalid game"
+        else:
+            winner = "invalid input"
+        return winner
 def rowchec_k(temp3):
-	'''
-	This is to check for rows
-	'''
-	if temp3[0][0] == temp3[0][1] == temp3[0][2]:
-		return temp3[0][0]
-	if temp3[1][0] == temp3[1][1] == temp3[1][2]:
-		return temp3[1][0]
-	if temp3[2][0] == temp3[2][1] == temp3[2][2]:
-		return temp3[2][0]
+    '''
+    This is to check for rows
+    '''
+    if temp3[0][0] == temp3[0][1] == temp3[0][2]:
+        return temp3[0][0]
+    if temp3[1][0] == temp3[1][1] == temp3[1][2]:
+        return temp3[1][0]
+    if temp3[2][0] == temp3[2][1] == temp3[2][2]:
+        return temp3[2][0]
 def colchec_k(temp3):
-	'''
-	This is to check cols
-	'''
-	if temp3[0][0] == temp3[1][0] == temp3[2][0]:
-		return temp3[0][0]
-	if temp3[0][1] == temp3[1][1] == temp3[2][1]:
-		return temp3[0][1]
-	if temp3[0][2] == temp3[1][2] == temp3[2][2]:
-		return temp3[0][2]
+    '''
+    This is to check cols
+    '''
+    if temp3[0][0] == temp3[1][0] == temp3[2][0]:
+        return temp3[0][0]
+    if temp3[0][1] == temp3[1][1] == temp3[2][1]:
+        return temp3[0][1]
+    if temp3[0][2] == temp3[1][2] == temp3[2][2]:
+        return temp3[0][2]
 def diag_check(temp3):
-	'''
-	This is to check diag
-	'''
-	if temp3[0][0] == temp3[1][1] == temp3[2][2]:
-		return temp3[0][0]
-	if temp3[0][2] == temp3[1][1] == temp3[2][0]:
-		return temp3[0][2]
+    '''
+    This is to check diag
+    '''
+    if temp3[0][0] == temp3[1][1] == temp3[2][2]:
+        return temp3[0][0]
+    if temp3[0][2] == temp3[1][1] == temp3[2][0]:
+        return temp3[0][2]
 def is_valid_input(temp3):
-	for i in range(3):
-		for j in range(3):
-			if temp3[i][j] == "x" or temp3[i][j] == "o" or temp3[i][j]== ".":
-				pass
-			else:
-				return False
-		return True
+    for i in range(3):
+        for j in range(3):
+            if temp3[i][j] == "x" or temp3[i][j] == "o" or temp3[i][j]== ".":
+                pass
+            else:
+                return False
+        return True
 def is_valid_game(temp3):
-	count = 0
-	sum1 = 0
-	for i in range(3):
-		for j in range(3):
-			if temp3[i][j] == "x":
-				count += 1
-			if temp3[i][j] == "o":
-				sum1 += 1
-	if count - sum1 == 1:
-		return True
-	return False
+    count = 0
+    sum1 = 0
+    for i in range(3):
+        for j in range(3):
+            if temp3[i][j] == "x":
+                count += 1
+            if temp3[i][j] == "o":
+                sum1 += 1
+    if count - sum1 == 1:
+        return True
+    return False
 
 
 
 def main():
-	res = tictactoe()
-	print(res)
+    res = tictactoe()
+    print(res)
 if __name__ == '__main__':
     main()
