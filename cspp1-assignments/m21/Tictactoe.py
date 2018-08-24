adaa6e60a85606db994c30c13a21ae231f600149
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-08-24 14:50:40 -0600

    " -> 0 of 14 passed. pylint: -2.29/10 "

--- cspp1-assignments/m21/Tictactoe.py
+++ cspp1-assignments/m21/Tictactoe.py
@@ -2,33 +2,36 @@
 
 
 def tictactoe():
-	# temp1 = []
-	# for i in range(3):
-	# 	temp2 = []
-	# 	for j in range(3):
-	# 		temp3 = input("enter input")
-	# 		temp2.append(temp3)
-	# temp1.append(temp2)
-	# print(temp1)
-	
 	temp3 = []
 	rowcheck = ''
+	colcheck = ''
+	diagcheck = ''
 	for i in range(3):
 		temp1 = input()
 		temp2 = temp1.split()
 		temp3.append(temp2)
 	rowcheck = rowchec_k(temp3)
+	colcheck = colchec_k(temp3)
+	diagcheck = diag_check(temp3)
 def rowchec_k(temp3):
 	if temp3[0][0] == temp3[0][1] == temp3[0][2]:
 		return temp3[0][0]
-
-
-	
-	
-
-
-
-
+	if temp3[1][0] == temp3[1][1] == temp3[1][2]:
+		return temp3[1][0]
+	if temp3[2][0] == temp3[2][1] == temp3[2][2]:
+		return temp3[2][0]
+def colchec_k(temp3):
+	if temp3[0][0] == temp3[1][0] == temp3[2][0]:
+		return temp3[0][0]
+	if temp3[0][1] == temp3[1][1] == temp3[2][1]:
+		return temp3[0][1]
+	if temp3[0][2] == temp3[1][2] == temp3[2][2]:
+		return temp3[0][2]
+def diag_check(temp3):
+	if temp3[0][0] == temp3[1][1] == temp3[2][2]:
+		return temp3[0][0]
+	if temp3[0][2] == temp3[1][1] == temp3[2][0]:
+		return temp3[0][2]
 
 
 def main():
