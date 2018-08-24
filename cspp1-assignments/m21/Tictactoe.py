796f85481ffda1b17de2529f3ff5ca93d1203d95
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-08-24 11:27:34 -0600

    " -> 0 of 1 passed. pylint: 6.25/10 "

---
+++ cspp1-assignments/m21/Tictactoe.py
@@ -0,0 +1,25 @@
+
+
+
+def tictactoe():
+	temp1 = []
+	for i in range(3):
+		temp2 = []
+		for j in range(3):
+			temp3 = input("enter input")
+			temp2.append(temp3)
+		temp1.append(temp2)
+	print(temp1)
+
+	
+
+
+
+
+
+
+def main():
+	res = print(tictactoe())
+	print(res)
+if __name__ == '__main__':
+    main()
