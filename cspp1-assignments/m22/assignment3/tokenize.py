d53ce47b6ab3a15927a7dba590f8c0d402e45efb
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-08-25 11:47:40 -0600

    " -> 0 of 4 passed. pylint: 0/0 "

--- cspp1-assignments/m22/assignment3/tokenize.py
+++ cspp1-assignments/m22/assignment3/tokenize.py
@@ -2,12 +2,31 @@
 Write a function to tokenize a given string and return a dictionary with the frequency of
 each word
 '''
-
 def tokenize(string):
-    pass
-            
+	pass    
 def main():
-    pass
-
+	'''
+	This is main function
+	'''
+    noof_lines = int(input())
+    for number_user in range(noof_lines):
+    	words_input = input()
+    	#temp10.append(words_input)
+    words_input = words_input.split()
+    temp_list = list(words_input)
+    dictionary_1 = {}
+    for word in temp_list:
+    	if word not in dictionary_1:
+    		dictionary_1[word] = 1
+    	else:
+    		dictionary_1[word] += 1
+    print(dictionary_1)
+    #print(temp)
+# 1
+# lorem ipsum porem lorem ipsum porem
+# {'lorem': 2, 'ipsum': 2, 'porem': 2}
+# 2
+# James, while John had had "had", had had "had had";
+# "had had" had had a better effect on the teacher.
 if __name__ == '__main__':
     main()
