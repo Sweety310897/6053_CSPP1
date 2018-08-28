3821429bc4de8ee9e1fdbfc251ec2757dc757e24
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-08-28 17:13:14 -0600

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- CSPP-2/cspp2-assignments/m2/Assignment-2/Solution.java
+++ CSPP-2/cspp2-assignments/m2/Assignment-2/Solution.java
@@ -6,32 +6,30 @@ import java.util.Scanner;
  * Class for solution.
  */
 public final class Solution {
-	/**
-	Do not modify this main function.
-	
-	@param      args  The arguments
-	*/
-	public static void main(final String[] args) {
-	/*
-	This is main function 
-	*/
-		Scanner scan = new Scanner(System.in);
-		int a = scan.nextInt();
-		int b = scan.nextInt();
-		int c = scan.nextInt();
-		rootsOfQuadraticEquation(a, b, c);
-	}
-	/**
-	Need to write the rootsOfQuadraticEquation function and print the output.
-	
-	@param      a     { parameter_description }
-	@param      b     { parameter_description }
-	@param      c     { parameter_description }
-	*/
-	public static void rootsOfQuadraticEquation(final int a, final int b, final int c)
-	{
-		double temp1 = (-b + (Math.sqrt((b * b) - (2 * 2 * a *c))))/(2 * a);
-		double temp2 = (-b - (Math.sqrt((b * b) - (2 * 2 * a *c))))/(2 * a);
-		System.out.println(temp1 + " " + temp2);
-	}
+    /**
+    Do not modify this main function
+    @param      args  The arguments
+    */
+    public static void main(final String[] args) {
+    /*
+    This is main function 
+    */
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        rootsOfQuadraticEquation(a, b, c);
+    }
+    /**
+    Need to write the rootsOfQuadraticEquation function and print the output.
+    @param      a     { parameter_description }
+    @param      b     { parameter_description }
+    @param      c     { parameter_description }
+    */
+    public static void rootsOfQuadraticEquation(final int a, final int b, final int c)
+    {
+        double temp1 = (-b + (Math.sqrt((b * b) - (2 * 2 * a *c))))/(2 * a);
+        double temp2 = (-b - (Math.sqrt((b * b) - (2 * 2 * a *c))))/(2 * a);
+        System.out.println(temp1 + " " + temp2);
+    }
 }
