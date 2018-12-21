5763d7884924f0541056381cc16b728edacfc28c
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-12-21 09:43:38 -0700

    "cspp1_m10_a1_1: 1/25 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 4/10 "

--- cspp1-assignments/remedial1/CSPP-1_Question/Solution.py
+++ cspp1-assignments/remedial1/CSPP-1_Question/Solution.py
@@ -4,7 +4,7 @@ def main():
     count = 0
     #print(len(inpu_t))
     for each in inpu_t:
-        if int(each) >=1 and int(each) <=9:
+        if int(each) >= 1 and int(each) <= 9:
             count = count + 1
     #print(count)
     if(count == 81):
