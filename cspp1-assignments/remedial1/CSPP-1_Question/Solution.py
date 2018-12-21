2cf85608a43cd6923db20076bee21993ecad1660
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-12-21 09:42:09 -0700

    "cspp1_m10_a1_1: 1/25 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 2/10 "

---
+++ cspp1-assignments/remedial1/CSPP-1_Question/Solution.py
@@ -0,0 +1,15 @@
+def main():
+    inpu_t = input()
+    #temp = 0123456789"
+    count = 0
+    #print(len(inpu_t))
+    for each in inpu_t:
+        if int(each) >=1 and int(each) <=9:
+            count = count + 1
+    #print(count)
+    if(count == 81):
+        print("Given sudoku is solved")
+
+     
+if __name__ == '__main__':
+    main()
