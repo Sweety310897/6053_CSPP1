85afff4fade5e91540803e3ecd5c72a31a71e74f
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-12-21 11:37:26 -0700

    "cspp1_m10_a1_1: 0/25 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 7/10 "

--- cspp1-assignments/remedial1/CSPP-1_Question/Solutio_n.py
+++ cspp1-assignments/remedial1/CSPP-1_Question/Solutio_n.py
@@ -1,3 +1,4 @@
+from collections import Counter
 '''this is main
     pgm'''
 #main function.
@@ -6,16 +7,25 @@ def main():
     { item_description }
     """
     inpu_t = input()
+    list1 = []
+    temp1 = 0
+    print(list1)
     #temp = 0123456789"
     coun_t = 0
-    #print(len(inpu_t))
+    print(len(inpu_t))
     for each in inpu_t:
         if int(each) >= 1 and int(each) <= 9:
             coun_t = coun_t + 1
     #print(count)
     if coun_t == 81:
         print("Given sudoku is solved")
-    temp = inpu_t.split(".")
-    print(temp)
+    # sudoku = []
+    # for _ in range(9):
+    #     row = input().split("\n")
+    #     print(row)
+    #     sudoku.append(row)
+     #   print(sudoku)
+    #print(check_sudoku(row))
+
 if __name__ == '__main__':
     main()
