887225799f3aa700424b7c1786739c725b334574
Sweety310897 <pulluri.shivani@msitprogram.net>
2019-01-05 10:31:25 -0700

    "cspp1_m10_a1_1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: -3/10 "

---
+++ cspp1-assignments/remedial/CSPP1Week-2Exam testcases/solution.py
@@ -0,0 +1,76 @@
+def main():
+	input1 = eval(input())
+	sum1 = 0
+	j = 0
+	i = 0
+	list1 = []
+	print(len(input1))
+	while i < len(input1):
+		for each in input1[i]:
+			sum1 += int(each)
+			print(sum1)
+		i += 1
+	# for each in input1:
+	# 	while j < len()
+	# 	len1 = len(input1[i])
+	# 	list1.append(input1[i])
+			# sum1 += float(each)
+			# print(sum1)
+			# break
+	# print(input1[0])
+	# x = list(map(int,input().split()))
+	# input2 = input1.split("]")
+	# print(input2[0])
+	# print(input2[1])
+	# tempstr = ""
+	# list1 = []
+	# temp1 = "["
+	# temp2 = "]"
+	# sum1 = 0
+	# i = 0
+	# j = 1
+	# count = 0
+	# while i < len(input1):
+	# 	if input1[i] == "[":
+	# 		count += 1
+	# 		print(count,"c{")
+	# 		while input1[j] != "]":
+	# 			sum1 += int(input1[j])
+			# while j < len(input1):
+			# 	if input1[j] == "]":
+			# 		tempval = input1[i+1:j]
+			# 		count = count - 1
+			# 		print(tempval,"tempval")
+			# 		print(count,"}")
+
+				# j += 1
+		
+			# print(count)
+			# while j < len(input1):
+			# 	if input1[j] == "]":
+			# 		count = count - 1
+			# 		print(count)
+			# 		tempval = input1[i+1:j]
+			# 		list1.append(tempval)
+					# print(tempval)
+
+				# j += 1
+		# i += 1
+	# print(list1)
+	# while i < len(input1):
+	# 	if input1[i] in "0123456789":
+	# 		list1.append(input1[i])
+			# sum1 += input1[i]
+			# print(sum1)
+		# i += 1
+	# print(sum1)
+	# print(list1)
+	# temp = int(list1)
+	# for i in range(len(list1)):
+	# 	sum1 += float(list1[i])
+	# 	print(sum1)
+
+
+	# input2 = input1.split("")
+if __name__ == '__main__':
+	main()
