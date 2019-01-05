9d60299a2657d33ad347f48d1367e02a74ed6b68
Sweety310897 <pulluri.shivani@msitprogram.net>
2019-01-05 12:21:50 -0700

    "cspp1_m10_a1_1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 3/10 "

--- cspp1-assignments/remedial/CSPP1Week-2Exam testcases/solution.py
+++ cspp1-assignments/remedial/CSPP1Week-2Exam testcases/solution.py
@@ -1,94 +1,46 @@
+def lister(l):
+    output = []
+    outer = []
+    templi = []
+    sum1 = 0
+    fsum = 0
+    for item in l:
+        if type(item) in [list, tuple, set]:
+            lister(item)
+        else:
+            output.append(item)
+            sum1 += item
+            outer.append(sum1)
+        # templi.append(sum1)
+    # for each in templi:
+    #     fsum += int(each)
+    # print(templi)
+    print(sum1)
+    # print(outer,"ou")
+    # print(fsum)
 def main():
-	input1 = eval(input())
-	sum1 = 0
-	j = 1
-	i = 0
-	count = 0
-	list1 = []
-	for i in input1:
-		# print(i,"i")
-		for j in i:
-			if j in "0123456789":
-
-				list1.append(j)
-			# print(input1[i][j])
-			# print(sum1)
-	print(list1)
-	# print(max(input1))
-	# while i < len(input1):
-	# 	for each in input1[i]:
-	# 		sum1 += int(input1[each])
-	# 		print(sum1)
-		# print(input1.split(","))
-		# while j != "]":
-		# print(length)
-		# for j in range(length):
-		# 	sum1 += input1[j]
-		# i += 1
-	# print(sum1)
-	# print(len(input1))
-	# print(list1)
-	# for each in input1:
-	# 	while j < len()
-	# 	len1 = len(input1[i])
-	# 	list1.append(input1[i])
-			# sum1 += float(each)
-			# print(sum1)
-			# break
-	# print(input1[0])
-	# x = list(map(int,input().split()))
-	# input2 = input1.split("]")
-	# print(input2[0])
-	# print(input2[1])
-	# tempstr = ""
-	# list1 = []
-	# temp1 = "["
-	# temp2 = "]"
-	# sum1 = 0
-	# i = 0
-	# j = 1
-	# count = 0
-	# while i < len(input1):
-	# 	if input1[i] == "[":
-	# 		count += 1
-	# 		print(count,"c{")
-	# 		while input1[j] != "]":
-	# 			sum1 += int(input1[j])
-			# while j < len(input1):
-			# 	if input1[j] == "]":
-			# 		tempval = input1[i+1:j]
-			# 		count = count - 1
-			# 		print(tempval,"tempval")
-			# 		print(count,"}")
-
-				# j += 1
-		
-			# print(count)
-			# while j < len(input1):
-			# 	if input1[j] == "]":
-			# 		count = count - 1
-			# 		print(count)
-			# 		tempval = input1[i+1:j]
-			# 		list1.append(tempval)
-					# print(tempval)
-
-				# j += 1
-		# i += 1
-	# print(list1)
-	# while i < len(input1):
-	# 	if input1[i] in "0123456789":
-	# 		list1.append(input1[i])
-			# sum1 += input1[i]
-			# print(sum1)
-		# i += 1
-	# print(sum1)
-	# print(list1)
-	# temp = int(list1)
-	# for i in range(len(list1)):
-	# 	sum1 += float(list1[i])
-	# 	print(sum1)
-
-
-	# input2 = input1.split("")
+    input1 = eval(input())
+    print(lister(input1))
+    # sum1 = 0
+    # j = 1
+    # i = 0
+    # count = 0
+    # temp = "0123456789"
+    # list1 = []
+    # res = []
+    # for val in input1:
+    #     if type(val) not in [list, set, tuple]:
+    #         list1.append(val)
+    #     else:
+    #         list1.extend(get_values(val))
+    # return res
+    # for i in input1:
+    #   for j in i:
+    #       list1.append(j)
+        
+            # print(sum1)
+    # print(list1)
+    
+    # input2 = input1.split("")
 if __name__ == '__main__':
-	main()
+    main()
