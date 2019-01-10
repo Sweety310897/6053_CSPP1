4b1063b41abf4aa996d267a6e0b230ac20310f01
Sweety310897 <pulluri.shivani@msitprogram.net>
2019-01-10 09:45:33 -0700

    "m19_a1: 0/13 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: -2/10 "

---
+++ cspp1-assignments/remedial1/Assignment-1/Solution.py
@@ -0,0 +1,47 @@
+def main():
+	loadq = input().split(" ")
+	print("|----------------|")
+	print("| Load Questions |")
+	print("|----------------|")
+	print(loadq[1], "are added to the quiz")
+	print("|------------|")
+	print("| Start Quiz |")
+	print("|------------|")
+	i = 0
+	list1 = []
+	while i < int(loadq[1]):
+		temp = input().split(":")
+		list1.append(temp)
+		i += 1
+	startq = input().split(" ")
+	# print(list1)
+	for i in range(len(list1)):
+		print(list1[i][0])
+		print(list1[i][1])
+	j = 0
+	list2 = []
+	while j < int(startq[1]):
+		tempstartq = input().split(" ")
+		list2.append(tempstartq)
+		j += 1
+	# print(list2)
+	scorereport = input()
+	print("|--------------|")
+	print("| Score Report |")
+	print("|--------------|")
+	marks = []
+	sum1 = 0
+	for i in range(len(list2)):
+		if list2[i][1] == list1[i][2]:
+			print(list1[i][0])
+			print("Correct Answer! - Marks Awarded:", list1[i][3])
+			sum1 += int(list1[i][3])
+			# print(sum1)
+		else:
+			# print(list1[i][4])
+			sum1 = sum1 + int(list1[i][4])
+			# print(sum1)
+	print("Total Score",sum1)
+
+if __name__ == '__main__':
+	main()
