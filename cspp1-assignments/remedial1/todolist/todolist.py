eecb1e75b40ed35acbb63313ac7823806dfbfd55
Sweety310897 <pulluri.shivani@msitprogram.net>
2019-01-23 15:44:31 -0700

    "final_exam_a1_1: 0/14 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: -3/10 "

--- cspp1-assignments/remedial1/todolist/todolist.py
+++ cspp1-assignments/remedial1/todolist/todolist.py
@@ -11,7 +11,6 @@ def addtask(temp):
 		str1 += "Not Urgent" + ", "
 	str1 += temp[6]
 	print(str1)
-
 def validate(temp):
 	# print(temp[6]=="todo")
 	if temp[1] == "":
@@ -22,27 +21,28 @@ def validate(temp):
 		raise Exception("Invalid status dud")
 	
 def main():
-	try :
-		sum1 = 0
-		while True:
-			try:
-				temp = input().split(",")
-				# print(temp)
-				if temp[0] != "print-todoist":
-					validate(temp)
-				if temp[0] == "task":
-					addtask(temp)
-				if temp[0] == "add-task":
-					addtask(temp)
-				if temp[6] == "todo":
-					sum1 += int(temp[3])
-				if temp[6] == "done":
-					sum1 = 0
-			except EOFError:
-				break
-		print(sum1)
-	except Exception as e:
-		print(e)
+	
+	sum1 = 0
+	while True:
+		try:
+			temp = input().split(",")
+			# print(temp)
+			# if temp[0] != "print-todoist":
+			validate(temp)
+			if temp[0] == "task":
+				addtask(temp)
+			if temp[0] == "add-task":
+				addtask(temp)
+			if temp[0] == "print-todoist":
+				print(temp)
+			if temp[0] == "total-time":
+				totaltime(temp)
+				
+		except EOFError:
+			break
+	print(sum1)
+	# except Exception as e:
+	# 	print(e)
 	
 if __name__ == '__main__':
 	main()
