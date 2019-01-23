9a1b957301b241b1c0d97cd893a9a01cb50e25d7
Sweety310897 <pulluri.shivani@msitprogram.net>
2019-01-23 16:13:06 -0700

    "final_exam_a1_1: 0/14 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 0/0 "

--- cspp1-assignments/remedial1/todolist/todolist.py
+++ cspp1-assignments/remedial1/todolist/todolist.py
@@ -14,14 +14,27 @@ def task(temp):
 		list1.append("Not Urgent")
 	list1.append(temp[6])
 	return list1
+def validate(temp):
+	if temp[1] == "":
+		raise Exception("Title not provided")
+	if int(temp[3]) < 0:
+		raise Exception("Invalid timeToComplete " + temp[3])
+	if temp[6] != "todo" and temp[6] != "done":
+		raise Exception("Invalid status dud")
+
 def main():
 	
 	temp = input().split(",")			
-	if temp[0] == "task":
+	try:
+		if temp[0] == "task":
+		validate(temp)
 		str1 = ""
 		temp1 = task(temp)
 		for each in temp1:
 			str1 += each + ", "
 		print(str1[0:len(str1)-2])
+	except Exception as e:
+		print(e)
+	
 if __name__ == '__main__':
 	main()
